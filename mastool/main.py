d4244733910768d58bc570b48d5ee679c245cd6e
Omar Abou Mrad <omar.aboumrad@gmail.com>
2015-10-09 10:14:08 +0300

    added a trivial argument parser

--- mastool/main.py
+++ mastool/main.py
@@ -4,12 +4,28 @@ from __future__ import print_function
 
 import sys
 import ast
+import argparse
 
 import practices
 
 
+def build_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('FILE',
+                        help='Target file to run mastool against')
+
+    parser.add_argument('--fail-hard', '-f',
+                        dest='fail_hard',
+                        action='store_true',
+                        default=False,
+                        help='exits with a none-zero status when issues found')
+
+    return parser
+
 def main():
-    code_file = sys.argv[1]
+    args = build_parser().parse_args()
+    code_file = args.FILE
+
     try:
         tree = ast.parse(open(code_file).read())
     except SyntaxError:
@@ -18,10 +34,15 @@ def main():
 
     paths = filter(lambda y: hasattr(y, 'label'), practices.__dict__.values())
 
+    caught = []
     for checker in paths:
         adherance = checker(tree)
+        caught.append(len(adherance) > 0)
         for lineno in adherance:
             print('{}:{}: {}'.format(code_file, lineno, checker.label))
+
+    if any(caught) and args.fail_hard:
+        sys.exit(1)
 
 
 if __name__ == '__main__':
