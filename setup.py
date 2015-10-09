e3aee9fa25a5d9c9fee5aa3a5c5272f24dbb09c6
Omar Abou Mrad <omar.aboumrad@gmail.com>
2015-10-09 10:23:10 +0300

    updated release

--- setup.py
+++ setup.py
@@ -4,18 +4,18 @@ from setuptools import setup
 setup(
     name = 'mastool',
     packages = ['mastool'],
-    version = '0.0.2',
+    version = '0.0.4',
     description = 'static analysis for bad or avoidable practices in python',
     author = 'Omar Abou Mrad',
     author_email = 'omar.aboumrad@gmail.com',
     url = 'https://github.com/omaraboumrad/mastool',
-    download_url = 'https://github.com/omaraboumrad/mastool/archive/0.0.2.tar.gz',
+    download_url = 'https://github.com/omaraboumrad/mastool/archive/0.0.4.tar.gz',
     keywords = ['static', 'analysis', 'practices'],
     classifiers = [],
 
     entry_points={
         'console_scripts': [
-            'mastool = mastool.mail:main',
+            'mastool = mastool.main:main',
         ]
     }
 )
